30be5a3541d5eb04611697800a5305efb8e51315
Peter Kreppein <pmkreppein@gmail.com>
2018-09-08 23:06:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return
 
 end
